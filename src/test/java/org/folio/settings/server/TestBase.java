22e4a903a30163eb7e522ddb20de67356d16f43d
Adam Dickmeiss <adam@indexdata.dk>
2025-04-28 10:05:13 +0200

    Unused import

--- src/test/java/org/folio/settings/server/TestBase.java
+++ src/test/java/org/folio/settings/server/TestBase.java
@@ -11,7 +11,6 @@ import io.vertx.core.json.JsonArray;
 import io.vertx.core.json.JsonObject;
 import io.vertx.ext.unit.TestContext;
 import io.vertx.ext.web.client.WebClient;
-import io.vertx.ext.web.client.predicate.ResponsePredicate;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.folio.okapi.common.ChattyHttpResponseExpectation;
